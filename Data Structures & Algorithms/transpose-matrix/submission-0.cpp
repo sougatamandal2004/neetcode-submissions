0b9656b5170fb24ac9ad02c0dbc9fe6a87769551
sougatagithubrit <158832491+sougatamandal2004@users.noreply.github.com>
2026-08-12 21:16:49 +0530

    Add: transpose-matrix - submission-0

---
+++ Data Structures & Algorithms/transpose-matrix/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<vector<int>> ans;
+        for(int i=0;i<n;i++){
+            vector<int> row;
+            for(int j=0;j<m;j++){
+                row.push_back(matrix[j][i]);
+            }
+            ans.push_back(row);
+        }
+        return ans;
+    }
+};
