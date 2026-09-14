b6971978e65dc2d906af5f8b8eca43bb7775a9d9
sougatagithubrit <158832491+sougatamandal2004@users.noreply.github.com>
2026-09-14 21:52:27 +0530

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        // Brute Force
+        // TC -> O(n^2), SC -> O(1)
+        int n = nums.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++) {
+                if(nums[i] == nums[j]) return true;
+            }
+        }
+        return false;
+    }
+}
