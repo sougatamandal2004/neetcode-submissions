322adb5ede72183bd905a296c30efb7d7d529488
sougatagithubrit <158832491+sougatamandal2004@users.noreply.github.com>
2026-09-15 20:16:27 +0530

    Add: longest-common-prefix - submission-4

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-4.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        // Brute Force -> go to each string and compare each character
+        // TC -> O(N*M), SC -> O(M) => M is the shortest common prefix
+        // String s = strs[0];
+        // for(int i=1;i<strs.length;i++){
+        //     int p = 0;
+        //     while(p < s.length() && p < strs[i].length() && (s.charAt(p) == strs[i].charAt(p))) {
+        //         p++;
+        //     }
+        //     s = s.substring(0, p);
+        //     if(s.isEmpty()) return "";
+        // }
+        // return s;
+
+        // Optimal One -> Sort the arraay compare only the first and the last String
+        // TC -> O(NlogN), SC -> O(M)
+        Arrays.sort(strs);
+        int n = strs.length - 1;
+        int x = 0;
+        while(x < strs[0].length() && (strs[0].charAt(x) == strs[n].charAt(x))) {
+            x++;
+        }
+        return strs[0].substring(0,x);
+    }
+}
